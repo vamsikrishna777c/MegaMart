0306d1b32252afadf96d9162557c50d8f6c8edc9
heman <heman@LAPTOP-JJ1STIEC>
2023-09-22 17:50:46 +0530

    initialchanges

---
+++ src/main/java/com/megaMart/MegaMartApplication.java
@@ -0,0 +1,15 @@
+package com.megaMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaMartApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/megaMart/MegaMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megaMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
