cc0c7aa47d4538dfcdfc9c34b9a0a44e88b1e918
shaik <shaik@LAPTOP-TQRT8F9R.bbrouter>
2023-09-22 23:52:43 +0530

    created Api class and modified application properties

--- src/main/java/com/megaMart/Controller/MegaMartAPI.java
+++ src/main/java/com/megaMart/Controller/MegaMartAPI.java
@@ -1,5 +1,31 @@
 package com.megaMart.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.megaMart.DTO.RegistrationDTO;
+import com.megaMart.exception.MegaMartException;
+import com.megaMart.service.MegaMartService;
+
+@RestController
+@RequestMapping
 public class MegaMartAPI {
+	@Autowired
+	private MegaMartService service;
+	@Autowired
+	private Environment environment;
+		@PostMapping("/user")
+	  public ResponseEntity<String> registration(@RequestBody RegistrationDTO registration )throws MegaMartException{ 
+		  service.registration(registration); 
+		  String successMessage=environment.getProperty("API.REGISTRATION_SUCCESS") + registration.getName();
+		  return new ResponseEntity<>(successMessage, HttpStatus.OK);
+		  }
+	 
 
 }
